b5eaca57c9c4e9e1a10985c9bc6a3863da453f0f
Paul Lu <sininglu@sandiego.edu>
2023-07-15 16:24:34 +0800

    added auth ui

--- src/routers.tsx
+++ src/routers.tsx
@@ -2,10 +2,23 @@
 
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
+import Signin from "./components/Signin";
+import Signup from "./components/Signup";
+import Navbar from "./components/Navbar";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <Navbar />,
+    children: [
+      {
+        path: "signin",
+        element: <Signin />,
+      },
+      {
+        path: "signup",
+        element: <Signup />,
+      },
+    ],
   },
 ]);
