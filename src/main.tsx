6e49066a3218e6f35a2c54cb68ae31348cb1c52c
Paul Lu <sininglu@sandiego.edu>
2023-08-25 12:35:06 +0800

    changed to production

--- src/main.tsx
+++ src/main.tsx
@@ -15,7 +15,8 @@ import {
 import { setContext } from "@apollo/client/link/context";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:4000/",
+  // uri: "http://localhost:4000/",
+  uri: "https://gql-blogging-website-production.up.railway.app/",
 });
 
 const authLink = setContext((_, { headers }) => {
